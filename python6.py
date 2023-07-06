e256aa44d5a3b73cda13bad2357ad83ac3cfd4b0
alfarabygit <alfarabyindra@gmail.com>
2023-07-06 21:14:01 +0700

    upload file latihan python

---
+++ python6.py
@@ -0,0 +1,59 @@
+#string di python
+text1='learning python is fun'
+text2="Keep spirit guys!"
+
+#multi line string
+text3= '''Hello guys, 
+welcome to python. 
+i hope you enjoying this'''
+text4= """dont' forget 
+share this content to your friends 
+and subscribers for more update content"""
+
+#concatenate strings
+a="Hello"
+b="everyone"
+text5=a+" "+b
+
+#escape strings
+text6="Let\'s learn python"
+
+#access string
+text7="Halo semua, namaku indra. aku fans cosplayer punipun dan aku ingin buat projek gallery fotonya.hahahahahahahaha...."
+
+#slicing string
+text8="Punipun fans"
+
+print(text1)
+print(text2)
+print(text3)
+print(text4)
+print(text5)
+print(text6)
+print(text7[10])
+print(text7[7])
+print(text7[-10])
+print(text7[-20])
+print(text8[4:2])
+print(text8[6:])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
