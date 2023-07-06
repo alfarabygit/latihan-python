27205e4aa0f6095abc219699fb6fc4f624439a63
alfarabygit <alfarabyindra@gmail.com>
2023-07-06 20:53:05 +0700

    upload file latihan python

---
+++ python4.py
@@ -0,0 +1,27 @@
+#tipe data pemrograman python
+
+#integer
+x=50
+#float
+y=75.4
+#string(single or double quote)
+fruit='manggo'
+name="punipun"
+singlesentence="python is fun to learn"
+doublesentence="Let's learn python!"
+#boolean
+isLearn=True
+isGaming=False
+#lists(can any type data)
+mylist=["Punipun","Nekonoi","Larissa","Azulchan"]
+
+#print type data
+print(x)
+print(y)
+print(fruit)
+print(name)
+print(singlesentence)
+print(doublesentence)
+print(isLearn)
+print(isGaming)
+print(mylist)
---
+++ python5.py
@@ -0,0 +1,62 @@
+#tipe data number di python
+#integer
+a=3
+b=7
+c=4
+#float
+d=5.0
+e=6.7
+f=85.3
+#complex number
+x=5j
+y=5.50j
+z=80.3j
+
+#mencetak tipe data number di python
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(x))
+print(type(y))
+print(type(z))
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(x)
+print(y)
+print(z)
+print(a+b)
+print(d-c)
+print(e*f)
+print(x/y)
+
+#round numbers
+pi=3.1415926535897
+two_decimals=round(pi, 2)
+three_decimals=round(pi, 3)
+four_decimals=round(pi, 4)
+print(two_decimals)
+print(three_decimals)
+print(four_decimals)
+
+#abs(absolute value)
+abs1= abs(-5)
+abs2= abs(68.6)
+print(abs1)
+print(abs2)
+
+#pow(pangkat)
+pow1=pow(5,4)
+pow2=pow(12,2)
+print(pow1)
+print(pow2)
+
+#divmod
+mod1=divmod(4,2)
+print(mod1)
